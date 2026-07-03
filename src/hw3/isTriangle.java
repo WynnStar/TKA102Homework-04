18e4123f1cb1a96197b8ed2dc9f50b8225b609e5
Wymm <chenyitorng@gmail.com>
2026-07-03 10:14:41 +0800

    維尼寫詩

--- src/hw3/isTriangle.java
+++ src/hw3/isTriangle.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 
 public class isTriangle {
 	public static void main(String[] args) {
-
+		
+		final int EXIT_CODE = 0;
 		System.out.println("\n• 請設計一隻程式,使用者輸入三個數字後,能判斷出是什麼三角形:\n");
 		Scanner sc = new Scanner(System.in);
 		while (true) {
-			System.out.println("  請輸入三角形的三個邊長(輸入0離開):");
+			System.out.print("  請輸入三角形的三個邊長(輸入0離開):");
 			int side1 = sc.nextInt();
 
-			if (side1 == 0) {
+			if (side1 == EXIT_CODE) {
 				break;
 
 			}
@@ -27,20 +28,25 @@ public class isTriangle {
 			int b = scArray[1];
 			int c = scArray[2];
 
-			if (((a + b) <= c) || a <= 0 || b <= 0 || c <= 0) {
-				System.out.println("　不是三角形\n");
+			
+			
+			
+			if (a == 4 && b == 6 && c==89) {
+				System.out.println("\n　　　維尼寫詩，維尼禁止，維尼做不可能的事！\n");
+			} else if (((a + b) <= c) || a <= 0 || b <= 0 || c <= 0) {
+				System.out.println("\n　　　　　　不是三角形\n");
 			} else if (a == b && b == c) {
-				System.out.println("　正三角形\n");
+				System.out.println("\n　　　　　　正三角形\n");
 //		}else if(((a*a+b*b)==(c*c))&&(a==b||b==c||a==c)) {
-//			System.out.println("等腰直角三角形\n");
+//			System.out.println("\n　　　　　等腰直角三角形\n");
 			} else if (a == b || b == c || a == c) {
-				System.out.println("　等腰三角形\n");
+				System.out.println("\n　　　　　　等腰三角形\n");
 			} else if ((a * a + b * b) == (c * c)) {
-				System.out.println("　直角三角形\n");
+				System.out.println("\n　　　　　　直角三角形\n");
 			} else if ((a * a + b * b) < (c * c)) {
-				System.out.println("　鈍角三角形\n");
+				System.out.println("\n　　　　　　鈍角三角形\n");
 			} else {
-				System.out.println("　銳角三角形\n");
+				System.out.println("\n　　　　　　銳角三角形\n");
 			}
 		}
 		sc.close(); // Java 建議關閉資源
