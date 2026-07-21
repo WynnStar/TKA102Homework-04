acc3ccb5721a0001a7d5b0043586d55edf8ce369
Wymm <chenyitorng@gmail.com>
2026-07-21 10:36:44 +0800

    改善int爆掉問題

--- src/hw5/InputInt.java
+++ src/hw5/InputInt.java
@@ -3,20 +3,26 @@ package hw5;
 import java.util.Scanner;
 
 public class InputInt {
-	
+
 	public static int inputInt(Scanner sc, String message) {
 
-	    while (true) {
+		while (true) {
 
-	        System.out.print(message);
-	        
-	        if (sc.hasNextInt()) {
-	            return sc.nextInt();
-	        }
-	        
+			System.out.print(message);
 
-	        System.out.println("\t錯誤：請輸入整數！\n");
-	        sc.next(); 
-	    }
+			if (sc.hasNextInt()) {
+				return sc.nextInt();
+			}
+
+			if (sc.hasNextLong()) {
+				System.out.println("\t錯誤：數值超出 int 可表示的範圍！");
+				sc.nextLong();
+			}
+
+			else {
+				System.out.println("\t錯誤：請輸入整數！\n");
+				sc.next();
+			}
+		}
 	}
 }
